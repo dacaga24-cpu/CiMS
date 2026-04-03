5aa647cca88ed8a370756ac0582e940515d33b3b
Edim Batalla <edim.batalla@gmail.com>
2026-04-03 17:10:45 +0200

    feat: add mysql setup

---
+++ backend/src/config/testDb.js
@@ -0,0 +1,13 @@
+const pool = require('./db');
+
+async function testDbConnection() {
+  try {
+    const connection = await pool.getConnection();
+    console.log('MySQL connection OK');
+    connection.release();
+  } catch (error) {
+    console.error('MySQL connection error:', error.message);
+  }
+}
+
+module.exports = testDbConnection;
