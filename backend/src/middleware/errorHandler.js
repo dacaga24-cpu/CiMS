eea7d1bc61d07587aa793cc4a8f5a41b3908070a
mabi24-lang <mabi24@bemen3.cat>
2026-04-03 15:48:45 +0200

    Upload Backend directories structure

--- backend/src/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-// errorHandler.js
-// Responsabilidad: Manejador global de errores de Express.
-// Centraliza todos los errores no capturados por los controladores.
-// Registra el error en consola y devuelve una respuesta JSON estandarizada.
-
-// Manejador global de errores (4 parametros obligatorios para que Express lo reconozca)
-function errorHandler(err, req, res, next) {}
-
-module.exports = errorHandler;
